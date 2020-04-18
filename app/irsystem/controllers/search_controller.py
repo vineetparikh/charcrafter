9c54d8ac40aeb41c0ddb50a0ab605612bbaf40ef
Matthew Shih <ms2628@cornell.edu>
2020-04-18 16:13:18 -0400

    added subclasses

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -11,6 +11,22 @@ import math
 project_name = "Character Crafter: Turn DnD Concepts to DnD Characters"
 net_id = "Vineet Parikh (vap43), Matthew Shih (ms2628), Eli Schmidt (es797), Eric Sunderland(evs37), Eric Chen(ebc48)"
 
+
+def query_toks_idf_norms(query, idf, tokenizer):
+	q_tokens = tokenizer.tokenize(query)
+	# Setting up query tf
+	query_tf = defaultdict(int)
+	for w in set(q_tokens):
+		query_tf[w] = q_tokens.count(w)
+	# Calculate query norms
+	query_norms = 0
+	terms = list(idf.keys())
+	for t in terms:
+		t_idf = idf[t]
+		query_norms += math.pow(query_tf[t] * t_idf, 2)
+	return q_tokens, query_tf, terms, query_norms
+
+
 def index_search(query, index, idf, doc_norms, tokenizer):
 	""" Search the collection of documents for the given query
 
@@ -39,34 +55,21 @@ def index_search(query, index, idf, doc_norms, tokenizer):
     Note:
 
     """
-
-	# YOUR CODE HERE
-	results = np.zeros(len(doc_norms))
-	answer = []
 	n_docs = len(doc_norms)
-	q_tokens = tokenizer.tokenize(query)
-	# Setting up query tf
-	query_tf = defaultdict(int)
-	for w in set(q_tokens):
-		query_tf[w] = q_tokens.count(w)
-	# Calculate query norms
-	query_norms = 0
-	terms = list(idf.keys())
-	for t in terms:
-		t_idf = idf[t]
-		query_norms += math.pow(query_tf[t] * t_idf, 2)
+	results = np.zeros(n_docs)
+	answer = []
+
+	q_tokens, query_tf, terms, query_norms = query_toks_idf_norms(query, idf, tokenizer)
 
 	for term in q_tokens:
 		if term in terms:
 			for doc_num, val in index[term]:
 				results[doc_num] += query_tf[term] * val * idf[term] * idf[term]
 
-	#     print(query_norms)
-	#     print(doc_norms)
-
 	for i in range(len(doc_norms)):
 		answer.append((results[i] / (math.sqrt(query_norms) * doc_norms[i]), i))
 	return sorted(answer, key=lambda x: x[0], reverse=True)
+
 
 def get_key(search_dict, val):
 	for key, value in search_dict.items():
@@ -74,8 +77,34 @@ def get_key(search_dict, val):
 			return key
 
 
-@irsystem.route('/', methods=['GET'])
+def build_inverted_index(msgs, tokenizer):
+	inv_idx = defaultdict(list)
+	for i, m in enumerate(msgs):
+		toks = tokenizer.tokenize(m)
+		for w in set(toks):
+			inv_idx[w].append((i, toks.count(w)))
+	return inv_idx
 
+
+def compute_idf(inv_idx, n_docs):
+	idf = dict()
+	for entry in inv_idx.keys():
+		len_of_list = len(inv_idx[entry])
+		idf[entry] = np.log2(n_docs / (1 + len_of_list))
+	return idf
+
+
+def compute_doc_norms(inv_idx, idf, n_docs):
+	doc_norms = np.zeros(n_docs)
+	terms = list(idf.keys())
+	for t in terms:
+		t_idf = idf[t]
+		for doc_num,val in inv_idx[t]:
+			doc_norms[doc_num] += math.pow(val*t_idf,2)
+	return np.sqrt(doc_norms)
+
+
+@irsystem.route('/', methods=['GET'])
 def search():
 	query = request.args.get('search')
 	if not query:
@@ -101,26 +130,36 @@ def search():
 
 		treebank_tokenizer = TreebankWordTokenizer()
 		class_flavor_list = list(class_flavor.values())
-		inv_idx = defaultdict(list)
-		for i, m in enumerate(class_flavor_list):
-			toks = treebank_tokenizer.tokenize(m)
-			for w in set(toks):
-				inv_idx[w].append((i, toks.count(w)))
-		print(inv_idx)
-		idf = dict()
-		for entry in inv_idx.keys():
-			len_of_list = len(inv_idx[entry])
-			idf[entry] = np.log2(len(class_flavor_list)/(1+len_of_list))
-		doc_norms = np.zeros(len(class_flavor_list))
-		# print(doc_norms)
-		terms = list(idf.keys())
-		for t in terms:
-			t_idf = idf[t]
-			for doc_num,val in inv_idx[t]:
-				doc_norms[doc_num] += math.pow(val*t_idf,2)
-		doc_norms = np.sqrt(doc_norms)
-		search_results = index_search(query, inv_idx, idf, doc_norms, treebank_tokenizer)
-		print(search_results)
-		results = [list(class_flavor.keys())[i] for score,i in search_results]
+		inv_idx = build_inverted_index(class_flavor_list, treebank_tokenizer)
+		idf = compute_idf(inv_idx, len(class_flavor_list))
+		doc_norms = compute_doc_norms(inv_idx, idf, len(class_flavor_list))
+		class_results = index_search(query, inv_idx, idf, doc_norms, treebank_tokenizer)
+
+		q_tokens, query_tf, terms, query_norms = query_toks_idf_norms(query, idf, treebank_tokenizer)
+		fin_sub_class_results = []
+		for score, doc_id in class_results:
+			class_list = list(class_flavor.keys())
+			subclass_list = class_subclass.get(class_list[doc_id])
+			sc_flavors = []
+			for sc in subclass_list:
+				sc_flavors.append(subclass_flavor.get(sc))
+			sc_inv_idx = build_inverted_index(sc_flavors, treebank_tokenizer)
+			sc_len = len(sc_flavors)
+			sc_idf = compute_idf(sc_inv_idx, sc_len)
+			sc_doc_norms = compute_doc_norms(sc_inv_idx, sc_idf, sc_len)
+			n_docs = len(sc_doc_norms)
+			results = np.zeros(n_docs)
+			sc_results = []
+			for term in q_tokens:
+				if term in terms:
+					for doc_num, val in sc_inv_idx[term]:
+						results[doc_num] += query_tf[term] * val * sc_idf[term] * sc_idf[term]
+			for i in range(len(sc_doc_norms)):
+				sc_results.append((results[i] / (math.sqrt(query_norms) * sc_doc_norms[i]), i))
+			for res, ind in sc_results:
+				fin_sub_class_results.append((score+res, class_list[doc_id] + ": " + subclass_list[ind]))
+			fin_sub_class_results = sorted(fin_sub_class_results, key=lambda x: x[0], reverse=True)
+
+		results = [res_class for score, res_class in fin_sub_class_results]
 		data = results
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
