6a5d150d41fab5f59e023614f7675a4f05bdd554
Elisa Schmidt <es797@cornell.edu>
2020-05-03 22:28:46 -0400

    revert search_controller

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -118,8 +118,7 @@ def search():
 			sqlquery2=subclass
 			sqlquery3="')"
 			fullquery=sqlquery+sqlquery2+sqlquery3
-			#socialrating = [r[0] for r in(db.engine.execute(fullquery))]
-			socialrating = []
+			socialrating = [r[0] for r in(db.engine.execute(fullquery))]
 			if(len(socialrating)==0):
 				socialrating=[0]
 
